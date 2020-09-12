69fd5c6d3a4805da99da138d9aae0b9a6e77afa1
Patrick <pl464@scarletmail.rutgers.edu>
2020-09-12 00:49:04 -0400

    Added pitch display

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 import Wad from 'web-audio-daw';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
-import Clock from './Clock';
+//import Clock from './Clock';
 
 function App() {
 
@@ -15,8 +15,11 @@ function App() {
   let tuner = new Wad.Poly();
 
   let logPitch = function(){
-    console.log(tuner.pitch, tuner.noteName);
-    request_id = requestAnimationFrame(logPitch);
+        if (typeof tuner.noteName !== "undefined") {
+            document.getElementById("pitch-display").innerHTML = tuner.noteName;
+        }
+        console.log(tuner.pitch, tuner.noteName);
+        request_id = requestAnimationFrame(logPitch);
   };
 
   function playvoice() {
@@ -24,17 +27,21 @@ function App() {
     tuner.add(voice);
     voice.play(); // You must give your browser permission to access your microphone before calling play().
     tuner.updatePitch(); // The tuner is now calculating the pitch and note name of its input 60 times per second. These values are stored in <code>tuner.pitch</code> and <code>tuner.noteName</code>.
+      
     requestAnimationFrame(logPitch);
   }
 
   function stopvoice() {
+    document.getElementById("pitch-display").innerHTML = "";
     voice.stop();
+    tuner.noteName = null;
     tuner.stopUpdatingPitch();
     cancelAnimationFrame(request_id);
   }
 
   return (
       <div>
+          <div id="pitch-display"></div>
           <h3>Have fun whistling!</h3>
           <Button variant="outline-secondary" className="mx-2" onClick={playvoice}>
             Start recording
