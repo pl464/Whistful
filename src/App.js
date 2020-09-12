a52269d9c8e5d8c511f8a8aef00c7d8538cc4924
therafu <therafuminqi@gmail.com>
2020-09-11 19:10:43 -0500

    Update button style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import React from 'react';
+import Wad from 'web-audio-daw';
+import Button from 'react-bootstrap/Button';
+
 function App() {
+
+  let request_id;
+
+  let saw = new Wad({source : 'sawtooth'});
+  let voice = new Wad({source : 'mic' });
+  let tuner = new Wad.Poly();
+
+  let logPitch = function(){
+    console.log(tuner.pitch, tuner.noteName);
+    request_id = requestAnimationFrame(logPitch);
+  };
+
+  function playsaw() {
+    saw.play();
+  }
+
+  function stopsaw() {
+    saw.stop();
+  }
+
+  function playvoice() {
+    tuner.setVolume(0); // If you're not using headphones, you can eliminate microphone feedback by muting the output from the tuner.
+    tuner.add(voice);
+    voice.play(); // You must give your browser permission to access your microphone before calling play().
+    tuner.updatePitch(); // The tuner is now calculating the pitch and note name of its input 60 times per second. These values are stored in <code>tuner.pitch</code> and <code>tuner.noteName</code>.
+    requestAnimationFrame(logPitch);
+  }
+
+  function stopvoice() {
+    voice.stop();
+    tuner.stopUpdatingPitch();
+    cancelAnimationFrame(request_id);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="outline-secondary" className="my-2" onClick={playsaw}>
+          Play test
+        </Button>
+        <Button variant="outline-secondary" className="my-2" onClick={stopsaw}>
+          Stop test
+        </Button>
+        <Button variant="outline-secondary" className="my-2" onClick={playvoice}>
+          Start recording
+        </Button>
+        <Button variant="outline-secondary" className="my-2" onClick={stopvoice}>
+          Stop recording
+        </Button>
       </header>
     </div>
   );
